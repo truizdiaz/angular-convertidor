bc4db489d72f8580f7f8fdddec84e334632adeb1
truizdiaz <tomiruizdiaz@gmail.com>
2020-11-27 15:08:35 -0300

    Convertidor HTML

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './componets/navbar/navbar.component';
+import { ConvertidorComponent } from './componets/convertidor/convertidor.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ConvertidorComponent
   ],
   imports: [
     BrowserModule
